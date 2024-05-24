0402404c99401353b9c441d5f227727110d53efd
Tshedza Musandiwa <tshedzamusandiwa@gmail.com>
2024-05-24 14:43:41 +0200

    Added review and confirmation screen

---
+++ absa test/Staff App/Staff App/Confirmation Screen/ConfirmationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConfirmationViewController.swift
+//  Staff App
+//
+//  Created by Tshedza Musandiwa on 2024/05/24.
+//
+
+import Foundation
+import UIKit
+
+class ConfirmationViewController: UIViewController {
+    
+}
---
+++ absa test/Staff App/Staff App/Review Screen/ReviewScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewScreenViewController.swift
+//  Staff App
+//
+//  Created by Tshedza Musandiwa on 2024/05/24.
+//
+
+import Foundation
+import UIKit
+
+class ReviewScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
