c112bab08d7360761e0754441b9bd36fb9d371a2
Tshedza Musandiwa <tshedzamusandiwa@gmail.com>
2024-05-24 13:55:02 +0200

    Added additional information screen

---
+++ absa test/Staff App/Staff App/Additional information screen/AdditionalInformationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdditionalInformationViewController.swift
+//  Staff App
+//
+//  Created by Tshedza Musandiwa on 2024/05/24.
+//
+
+import Foundation
+import UIKit
+
+class AdditionalInformationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ absa test/Staff App/Staff App/Additional information screen/AdditionalInformationViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AdditionalInformationViewModel.swift
+//  Staff App
+//
+//  Created by Tshedza Musandiwa on 2024/05/24.
+//
+
+import Foundation
+
+class AdditionalInformationViewModel {
+    
+}
